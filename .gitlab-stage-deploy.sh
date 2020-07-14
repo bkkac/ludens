7dc8f0996f793095204697cc056e720188fe7f9e
thanakon <thanakonngam@gmail.com>
2020-07-14 14:29:09 +0700

    loop cmd in ssh

---
+++ .gitlab-stage-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Get servers list
+set -f
+string=$STAGE_DEPLOY_SERVER
+array=(${string//,/ })
+
+#Iterate servers for deploy and pull last commit
+for i in "${!array[@]}"; do
+  echo "Deploying information to EC2 and Gitlab"
+  echo "Deploy project on server ${array[i]}"
+  ssh ubuntu@${array[i]} "cd  /var/www/ && touch test"
+done
